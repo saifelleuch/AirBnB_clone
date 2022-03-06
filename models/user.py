7e95dbef6b2deb20aed6c10404cec5ad3286446e
threadsv1 <majdimed3@gmail.com>
2022-03-06 22:34:26 +0100

    edited

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""User Class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+        Class User that inherits from BaseModel.
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

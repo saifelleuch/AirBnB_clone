766b88e0622e0424d67f7d03b419b0952a3d0245
threadsv1 <majdimed3@gmail.com>
2022-03-06 23:25:16 +0100

    edited

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,56 @@
 #!/usr/bin/python3
-""" Class Model Module"""
-from asyncio.windows_events import NULL
-from uuid import uuid
+"""
+base model
+"""
+import models
+import uuid
 from datetime import datetime
-
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """ Define all common attributes/methods for other classes """
+    """
+    Base model that defines all common attributes/methods for all the classes
+    """
+
     def __init__(self, *args, **kwargs):
-        """ Init Base Model """
-        if kwargs is not NULL:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    new_value = datetime.strptime(value, time)
-
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        """ Print: [<class name>] (<self.id>) <self.__dict__> """
-        print("[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
-    def save(self):
-        """  Updates the public instance attribute \
-            updated_at with the current datetime
         """
+        __init__ methode
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def to_dict(self):
-        """ Returns a dictionary containing \
-            all keys/values of __dict__ of the instance
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, time_format)
+                if k != "__class__":
+                    setattr(self, k, v)
+        else:
+            models.storage.new(self)
+
+    def __str__(self):
         """
-        dict = self.___dict__.copy()
-        dict["created_at"] = self.created_at.isoformat()
-        dict["updated_at"] = self.updated_at.isoformat()
-        dict["__class__"] = self.__class__.__name__
-        return dict
+        print with this format : [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        """
+        d = dict(**self.__dict__)
+        d['__class__'] = str(type(self).__name__)
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
+        return (d)
--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """
-        Class User that inherits from BaseModel.
+        Class User that inherits from BaseModel
     """
     email = ""
     password = ""

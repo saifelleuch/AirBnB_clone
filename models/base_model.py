fbadebb82ef3eeb47fe57915631f49e44a9883ee
saifelleuch <sa2014if@gmail.com>
2022-03-06 01:01:01 +0100

    fixed

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,17 @@ from asyncio.windows_events import NULL
 from uuid import uuid
 from datetime import datetime
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseModel:
     """ Define all common attributes/methods for other classes """
-
     def __init__(self, *args, **kwargs):
         """ Init Base Model """
         if kwargs is not NULL:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    new_value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    new_value = datetime.strptime(value, time)
 
         else:
             self.id = str(uuid.uuid4())
@@ -20,17 +22,18 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Print: [<class name>] (<self.id>) <self.__dict__> 
-        """
-        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        """ Print: [<class name>] (<self.id>) <self.__dict__> """
+        print("[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """  Updates the public instance attribute updated_at with the current datetime
+        """  Updates the public instance attribute \
+            updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
-        """ Returns a dictionary containing all keys/values of __dict__ of the instance
+        """ Returns a dictionary containing \
+            all keys/values of __dict__ of the instance
         """
         dict = self.___dict__.copy()
         dict["created_at"] = self.created_at.isoformat()
